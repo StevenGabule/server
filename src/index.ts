986c66733e3628d19ab92c5d460b2a2d3eac5a9d
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-08-30 11:11:39 +0800

    module 3 done - apollo setup for server

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import { listings } from "./listings";
+import { resolvers, typeDefs } from "./graphql";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
 
 const app = express();
 const port = 9000;
 
-const one = 1;
-const two = 2;
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.use(express.json());
-
-app.get("/", (_req, res) => res.send(`yep!, ${one + two}`));
-
-// listings route
-app.get("/listings", (_, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listings", (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.status(200).send(listings.splice(i, 1));
-    }
-  }
-  return res.status(404).send("failed to delete listing");
-});
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port, () => console.log(`App listening to: ${port}`));
